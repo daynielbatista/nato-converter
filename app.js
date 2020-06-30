9f9161a3ecc7f85254bb5ad81798ba1af96e94df
Dayniel <daynielbatista@gmail.com>
2020-07-01 00:28:10 +0100

    Day 2

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 (function(){
 
-  const cases = {
+  /*const cases = {
     'a': 'Alpha',
     'b': 'Bravo',
     'c': 'Charlie',
@@ -38,8 +38,65 @@
     '8': 'Eight',
     '9': 'Niner',
     ' ': ' • '
-  };
+  }; */
 
-})
+  function convertTextToNato()
+{
+    var textToConvert = document.getElementById('textToConvert').value;
+    document.getElementById('conversionResults').innerHTML = textToNato(textToConvert);
+}
+
+
+  for (var i=0; i < text.length; i++)
+  {
+      switch (text.charAt(i))
+      {
+          case 'A': results = results + 'alfa '; break;
+          case 'B': results = results + 'bravo '; break;
+          case 'C': results = results + 'charlie '; break;
+          case 'D': results = results + 'delta '; break;
+          case 'E': results = results + 'echo '; break;
+          case 'F': results = results + 'foxtrot '; break;
+          case 'G': results = results + 'golf '; break;
+          case 'H': results = results + 'hotel '; break;
+          case 'I': results = results + 'india '; break;
+          case 'J': results = results + 'juliett '; break;
+          case 'K': results = results + 'kilo '; break;
+          case 'L': results = results + 'lima '; break;
+          case 'M': results = results + 'mike '; break;
+          case 'N': results = results + 'november '; break;
+          case 'O': results = results + 'oscar '; break;
+          case 'P': results = results + 'papa '; break;
+          case 'Q': results = results + 'quebec '; break;
+          case 'R': results = results + 'romeo '; break;
+          case 'S': results = results + 'sierra '; break;
+          case 'T': results = results + 'tango '; break;
+          case 'U': results = results + 'uniform '; break;
+          case 'V': results = results + 'victor '; break;
+          case 'W': results = results + 'whiskey '; break;
+          case 'X': results = results + 'xray '; break;
+          case 'Y': results = results + 'yankee '; break;
+          case 'Z': results = results + 'zulu '; break;
+          case '0': results = results + 'Zero '; break;
+          case '1': results = results + 'Won '; break;
+          case '2': results = results + 'Two '; break;
+          case '3': results = results + 'Three '; break;
+          case '4': results = results + 'Four '; break;
+          case '5': results = results + 'Five '; break;
+          case '6': results = results + 'Six '; break;
+          case '7': results = results + '7 '; break;
+          case '8': results = results + 'Eight '; break;
+          case '9': results = results + 'Niner '; break;
+          case ' ': results = results + ' • '; break;
+          case ' ': results = results + newline + newline; break;
+          default: results = results + text.charAt(i) + ' ';
+      }
+  }
+  
+  return results;
+}
+
+
+);
 
 
